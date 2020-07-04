c5b937c7e45033d84fec38ed820d1adce439b4d2
wisehead <alex.chenhui@gmail.com>
2020-07-04 21:49:48 +0800

    vscode demo greetings

---
+++ workspace/greetings/target/debug/deps/greetings.d
@@ -0,0 +1,5 @@
+/Users/chenhui13/github/rust_lib/workspace/greetings/target/debug/deps/greetings: src/main.rs
+
+/Users/chenhui13/github/rust_lib/workspace/greetings/target/debug/deps/greetings.d: src/main.rs
+
+src/main.rs:
---
+++ workspace/greetings/target/debug/greetings.d
@@ -0,0 +1,1 @@
+/Users/chenhui13/github/rust_lib/workspace/greetings/target/debug/greetings: /Users/chenhui13/github/rust_lib/workspace/greetings/src/main.rs
