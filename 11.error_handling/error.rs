af2b0d0df529a30092315e9bcc282c51828f371a
wisehead <alex.chenhui@gmail.com>
2020-07-05 13:55:05 +0800

    handle error.

---
+++ 11.error_handling/error.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+
+fn main() {
+    let f = File::open("hello.txt");
+    match f {
+        Ok(file) => {
+            println!("File opened successfully.");
+        },
+        Err(err) => {
+            println!("Failed to open the file.");
+        }
+    }
+}
