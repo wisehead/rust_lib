741e6c565bbcc3003b0818db20e1c07a86299fc3
wisehead <alex.chenhui@gmail.com>
2020-07-05 14:22:08 +0800

    pass_error

---
+++ 11.error_handling/pass_error.rs
@@ -0,0 +1,18 @@
+fn f(i: i32) -> Result<i32, bool> {
+    if i >= 0 { Ok(i) }
+    else { Err(false) }
+}
+
+fn g(i: i32) -> Result<i32, bool> {
+    let t = f(i)?;
+    Ok(t) // 因为确定 t 不是 Err, t 在这里已经是 i32 类型
+}
+
+fn main() {
+    let r = g(10000);
+    if let Ok(v) = r {
+        println!("Ok: g(10000) = {}", v);
+    } else {
+        println!("Err");
+    }
+}
