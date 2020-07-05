664b0b37ed9e0f3a79457022603f45c091736e07
wisehead <alex.chenhui@gmail.com>
2020-07-05 11:16:31 +0800

    09.enum

---
+++ 09.enum/match.rs
@@ -0,0 +1,18 @@
+fn main() {
+    enum Book {
+        Papery {index: u32},
+        Electronic {url: String},
+    }
+   
+    let book = Book::Papery{index: 1001};
+    let ebook = Book::Electronic{url: String::from("url...")};
+   
+    match book {
+        Book::Papery { index } => {
+            println!("Papery book {}", index);
+        },
+        Book::Electronic { url } => {
+            println!("E-book {}", url);
+        }
+    }
+}
