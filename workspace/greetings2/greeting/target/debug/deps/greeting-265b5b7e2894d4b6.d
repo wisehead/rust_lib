7e3b13cf3c413d43943542a373d9bf47067be38d
Hui Chen <alex.chenhui@gmail.com>
2023-04-24 11:50:58 +0800

    setup vs code env

---
+++ workspace/greetings2/greeting/target/debug/deps/greeting-265b5b7e2894d4b6.d
@@ -0,0 +1,5 @@
+/Users/wisehead/github/rust_lib/workspace/greetings2/greeting/target/debug/deps/greeting-265b5b7e2894d4b6: src/main.rs
+
+/Users/wisehead/github/rust_lib/workspace/greetings2/greeting/target/debug/deps/greeting-265b5b7e2894d4b6.d: src/main.rs
+
+src/main.rs:
---
+++ workspace/greetings2/greeting/target/debug/greeting.d
@@ -0,0 +1,1 @@
+/Users/wisehead/github/rust_lib/workspace/greetings2/greeting/target/debug/greeting: /Users/wisehead/github/rust_lib/workspace/greetings2/greeting/src/main.rs
