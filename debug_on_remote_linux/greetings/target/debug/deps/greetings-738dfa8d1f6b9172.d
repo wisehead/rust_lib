276f9b7b3720723b727d772d471c0ffc3bfb6f60
wisehead <alex.chenhui@gmail.com>
2023-04-24 17:05:17 +0800

    debug on remote linux

---
+++ debug_on_remote_linux/greetings/target/debug/deps/greetings-738dfa8d1f6b9172.d
@@ -0,0 +1,5 @@
+/home/chenhui/github/rust_lib/test/greetings/target/debug/deps/greetings-738dfa8d1f6b9172: src/main.rs
+
+/home/chenhui/github/rust_lib/test/greetings/target/debug/deps/greetings-738dfa8d1f6b9172.d: src/main.rs
+
+src/main.rs:
